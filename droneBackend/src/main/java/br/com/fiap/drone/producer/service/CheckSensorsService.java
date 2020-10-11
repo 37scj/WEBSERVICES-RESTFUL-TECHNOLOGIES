0a8339cae6f717d0d282a1d2f1a453901810d2e2
Lucas Rodrigues <lucasrodriguesdonascimento@MacBook-Pro.local>
2020-10-11 14:06:42 -0300

    Limpando

--- droneBackend/src/main/java/br/com/fiap/drone/producer/service/CheckSensorsService.java
+++
@@ -1,5 +0,0 @@
-package br.com.fiap.drone.producer.service;
-
-public interface CheckSensorsService {
-    public void checkSensors();
-}
--- droneBackend/src/main/java/br/com/fiap/drone/producer/service/MailService.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.drone.producer.service;
-
-public interface MailService {
-
-    public void sendEmail(String msgBody);
-
-}
